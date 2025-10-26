4d0e2cd5c7a05d9e3893f40b9214b77fc8087d28
Mich-O <m.odhiambo@alustudent.com>
2025-10-26 21:48:11 +0200

    PEP-8 Validation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 Script that, using a REST API, returns information about
 an employee’s TODO list progress.
@@ -8,18 +7,31 @@ an employee’s TODO list progress.
 import requests
 import sys
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
-    employee_id = sys.argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
-    user = requests.get("{}/users/{}".format(base_url, employee_id)).json()
-    todos = requests.get("{}/todos?userId={}".format(base_url, employee_id)).json()
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
 
-    done_tasks = [t for t in todos if t.get("completed")]
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = "{}/users/{}".format(base_url, employee_id)
+    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
+
+    user = requests.get(user_url).json()
+    todos = requests.get(todos_url).json()
+
+    done_tasks = [task for task in todos if task.get("completed")]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(done_tasks), len(todos)))
+        user.get("name"),
+        len(done_tasks),
+        len(todos))
+    )
+
     for task in done_tasks:
         print("\t {}".format(task.get("title")))
