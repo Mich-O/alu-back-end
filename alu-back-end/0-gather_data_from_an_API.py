ba15d9ed3379e0d6eb1ecc25865c81ef15b74407
Mich-O <m.odhiambo@alustudent.com>
2025-10-26 21:20:52 +0200

    Gather data from API

---
+++ alu-back-end/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    base_url = "https://jsonplaceholder.typicode.com"
+    user = requests.get("{}/users/{}".format(base_url, employee_id)).json()
+    todos = requests.get("{}/todos?userId={}".format(base_url, employee_id)).json()
+
+    done_tasks = [t for t in todos if t.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(done_tasks), len(todos)))
+    for task in done_tasks:
+        print("\t {}".format(task.get("title")))
